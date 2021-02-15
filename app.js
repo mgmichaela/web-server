0a92b601bb5622aa5909449aa33b44c1c7a53a34
Michaela Guzmická <mgmichaelaguzmicka@gmail.com>
2021-02-15 15:56:27 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const port = 3000;
+const server = http.createServer(function(req, res) {
+    res.write('Welcome to the server!');
+    res.end();
+});
+
+server.listen(port, function(error) {
+ if(error) {
+    console.log('Something went wrong', error);
+ } else {
+     console.log('No error occured' + port);
+ }
+});
