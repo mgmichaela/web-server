04a4a833db72a083a05dc889492e67a5b21a1867
Michaela Guzmická <mgmichaelaguzmicka@gmail.com>
2021-02-15 16:08:40 +0100

    import fs, read html

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const http = require("http");
+const fs = require("fs"); // handle all files
 const port = 3000;
 const server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("index.html", function (error, data) {
+    if (error) {
+      res.writeHead(404);
+      res.write("File not found");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
   res.write("Welcome to the server!");
   res.end();
 });
